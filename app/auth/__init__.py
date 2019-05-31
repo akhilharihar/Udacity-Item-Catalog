e709546b928633ca1c0ad3f1e8a72596de0eabf7
akhil harihar <akhil01@live.com>
2019-05-31 16:26:02 +0530

    login via google

--- app/auth/__init__.py
+++ app/auth/__init__.py
@@ -1,5 +1,8 @@
 from .manager import login_manager
+from .oauth_providers.google import GoogleAuth
 
-installed_auth_providers = {}
+installed_auth_providers = {
+    'google': GoogleAuth
+}
 
 __all__ = ['login_manager',  'installed_auth_providers']
