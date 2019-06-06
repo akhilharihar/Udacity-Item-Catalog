4bf19dc37fa5d5223f9632bd0e8ad4505659e1eb
akhil harihar <akhil01@live.com>
2019-06-06 14:08:30 +0530

    remove url trailing slash, url helpers

--- app/utils/__init__.py
+++ app/utils/__init__.py
@@ -1,6 +1,6 @@
-from .urls import Path
+from app.config.route import Path, Resource
 from .responses import render, response, json_response
 from .hashes import Hashes, AbstractHashID
 
-__all__ = ['Path', 'render', 'response', 'json_response', 'Hashes',
+__all__ = ['Path', 'Resource', 'render', 'response', 'json_response', 'Hashes',
            'AbstractHashID']
