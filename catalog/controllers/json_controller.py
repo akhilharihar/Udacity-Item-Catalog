1f0184357df62d3080cb63b26b64ff8d32ff352d
akhil harihar <akhil01@live.com>
2019-06-07 13:02:03 +0530

    api controller

---
+++ catalog/controllers/json_controller.py
@@ -0,0 +1,40 @@
+from .category_controller import CategoryController
+from .item_controller import ItemController
+from app.utils import json_response
+from flask import request
+
+
+class CategoryAPI:
+    @staticmethod
+    def index():
+        data = CategoryController.index()
+        return json_response(data)
+
+
+class ItemAPI:
+    @staticmethod
+    def index():
+        page = 1
+        per_page = 25
+
+        if request.args.get('page'):
+            page = request.args['page']
+
+        if request.args.get('per_page'):
+            per_page = request.args['per_page']
+
+        category = request.args.get('category', None)
+
+        if category:
+            category = CategoryController.decode_id(category)
+
+        data = ItemController.index(category_id=category, page=page,
+                                    per_page=per_page)
+
+        return json_response(data)
+
+    @staticmethod
+    def get(item_id):
+        data = ItemController.get(ItemController.decode_id(item_id))
+
+        return json_response(data)
