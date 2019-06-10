508182874a88cd6e6794a404bfcce8d3b181aed4
akhil harihar <akhil01@live.com>
2019-06-10 10:06:07 +0530

    authorisation

--- catalog/controllers/page_controller.py
+++ catalog/controllers/page_controller.py
@@ -1,25 +1,26 @@
-from .category_controller import CategoryController
-from .item_controller import ItemController
-from app.utils import response, render
-from flask import request, flash, redirect, url_for
+from .category_controller import CategoryController as CCT
+from .item_controller import ItemController as ICT
+from app.utils import response, render, url
+from flask import request, flash, redirect
+from flask_login import login_required, current_user
 
 
 class PageController:
 
     @staticmethod
     def index(category_name=None):
-        categories = CategoryController.index()
+        categories = CCT.index()
 
         page = request.args.get('page', 1)
         per_page = request.args.get('per_page', 15)
 
         if category_name:
             url_category = " ".join(category_name.split('_'))
-            db_category = CategoryController.get_by_name(url_category)
-            db_items = ItemController.index(db_category.id, page=page,
-                                            per_page=per_page)
+            db_category = CCT.get_by_name(url_category)
+            db_items = ICT.index(db_category.id, page=page,
+                                 per_page=per_page)
         else:
-            db_items = ItemController.index(page=page, per_page=per_page)
+            db_items = ICT.index(page=page, per_page=per_page)
 
         return response(render('catalog/items_list.html',
                                current_category=category_name,
@@ -27,57 +28,72 @@ class PageController:
 
     @staticmethod
     def show_item(item_id):
-        item = ItemController.get(ItemController.decode_id(item_id))
+        item = ICT.get(ICT.decode_id(item_id))
         return response(render('catalog/show_item.html', item=item))
 
     @staticmethod
+    @login_required
     def delete_item(item_id):
-        item = ItemController.get(ItemController.decode_id(item_id), True)
+        item = ICT.get(ICT.decode_id(item_id), True)
 
-        result = ItemController.delete(item)
+        if current_user.id != item.user_id:
+            flash('No permission grant to delete this item.', 'form_error')
+            return redirect(url('catalog.show_item',
+                                item_id=item.hash_id))
+
+        result = ICT.delete(item)
 
         if result['result']:
             flash(result['message'], 'success')
         else:
             flash(result['message'], 'form_error')
 
-        return redirect(url_for('catalog.catalog'))
+        return redirect(url('catalog.catalog'))
 
     @staticmethod
+    @login_required
     def create_item():
         if request.method == 'POST':
             return PageController.store_item()
 
-        categories = CategoryController.index()
+        categories = CCT.index()
         return response(render('catalog/add_item.html',
                                categories=categories))
 
     @staticmethod
     def store_item():
-        result = ItemController.store()
+        result = ICT.store(current_user.id)
 
         if result['result']:
             message = 'Item {} added \
                 successfully'.format(result['message'].name)
 
             flash(message, 'success')
-            return redirect(url_for('catalog.show_item',
-                                    item_id=result['message'].hash_id))
+            return redirect(url('catalog.show_item',
+                                item_id=result['message'].hash_id))
 
         else:
             flash(result['message'], 'form_error')
             print(result['message'])
 
-            return redirect(url_for('catalog.create_item'))
+            return redirect(url('catalog.create_item'))
 
     @staticmethod
+    @login_required
     def edit_item(item_id):
+        item = ICT.get(ICT.decode_id(item_id), True)
+
+        if current_user.id != item.user_id:
+            flash('No permission grant to modify this item.', 'form_error')
+            return redirect(url('catalog.show_item',
+                                item_id=item.hash_id))
+
         if request.method == 'POST':
-            item = ItemController.get(ItemController.decode_id(item_id), True)
             return PageController.update_item(item)
 
-        item = ItemController.get(ItemController.decode_id(item_id))
-        categories = CategoryController.index()
+        item = ICT.item_to_dict(item)
+
+        categories = CCT.index()
 
         return response(render('catalog/edit_item.html', item=item,
                                categories=categories))
@@ -85,19 +101,19 @@ class PageController:
     @staticmethod
     def update_item(item):
 
-        result = ItemController.update(item)
+        result = ICT.update(item)
 
         if result['result']:
             message = 'Item {} updated \
                 successfully'.format(result['message'].name)
 
             flash(message, 'success')
-            return redirect(url_for('catalog.show_item',
-                                    item_id=result['message'].hash_id))
+            return redirect(url('catalog.show_item',
+                                item_id=result['message'].hash_id))
 
         else:
             flash(result['message'], 'form_error')
 
             print(result['message'])
 
-            return redirect(url_for('catalog.edit_item', item_id=item.hash_id))
+            return redirect(url('catalog.edit_item', item_id=item.hash_id))
